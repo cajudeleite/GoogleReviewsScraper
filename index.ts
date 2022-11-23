a17af0871c563c672087fa3c921b870ff9399233
Caju <nathandebaz266@gmail.com>
2022-11-23 15:59:02 +0100

    entering the review page

--- index.ts
+++ index.ts
@@ -22,38 +22,65 @@ const getReviews = async (searchQuery: string) => {
 
     const res = await checkIfInCookiePage(page);
 
-    if (res) console.log("In cookie");
-    else console.log("Not in cookie");
+    if (!res) throw new Error("Didn't start at cookie page");
 
     const buttonSelector = "button";
     await page.waitForSelector(buttonSelector);
-    console.log("Found button");
 
-    const [response] = await Promise.all([
+    await Promise.all([
       page.waitForNavigation(), // The promise resolves after navigation has finished
       page.click(buttonSelector), // Clicking the link will indirectly cause a navigation
     ]);
 
-    console.log(response);
-
     const resp = await checkIfInCookiePage(page);
 
-    if (resp) console.log("In cookie");
-    else console.log("Not in cookie");
+    if (resp) throw new Error("Didn't pass the cookie page");
+    else console.log("Passed the cookie page");
 
     // Wait for the results page to load and display the results.
     const resultsSelector = ".hfpxzc";
     await page.waitForSelector(resultsSelector);
 
-    console.log("Found results");
-
-    // Extract the results from the page.
-    const titles = await page.evaluate((resultsSelector) => {
-      return [...document.querySelectorAll(resultsSelector)].map((anchor) => anchor.ariaLabel);
+    // Extract the urls from the restaurants
+    const restaurants = await page.evaluate((resultsSelector) => {
+      return [...document.querySelectorAll(resultsSelector)].map((anchor) => ({
+        name: anchor.ariaLabel,
+        url: (anchor as HTMLLinkElement).href,
+      }));
     }, resultsSelector);
 
+    for (let i = 0; i < restaurants.length; i++) {
+      const restaurant = restaurants[i];
+
+      console.log(`Fetching ${restaurant.name}`);
+
+      await Promise.all([
+        page.waitForNavigation(), // The promise resolves after navigation has finished
+        page.goto(restaurant.url),
+      ]);
+
+      const moreReviewsButtonSelector = ".M77dve";
+      await page.waitForSelector(moreReviewsButtonSelector);
+
+      // Find more reviews button
+      await page.evaluate((moreReviewsButtonSelector) => {
+        [...document.querySelectorAll(moreReviewsButtonSelector)].forEach((anchor) => {
+          if (anchor.ariaLabel?.includes("Plus d'avis")) (anchor as HTMLButtonElement).click();
+        });
+      }, moreReviewsButtonSelector);
+
+      // await Promise.all([
+      //   page.waitForNavigation(), // The promise resolves after navigation has finished
+      // ]);
+
+      const rating = ".fontDisplayLarge";
+      await page.waitForSelector(rating);
+      const rate = await page.evaluate((rating) => document.querySelector(rating)?.textContent, rating);
+      console.log(rate);
+    }
+
     // Print all the files.
-    console.log(titles.join("\n"));
+    // console.log(urls.join("\n"));
 
     await browser.close();
   } catch (error) {
